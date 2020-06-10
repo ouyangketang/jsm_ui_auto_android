701b9bc1204291b06136470f871ec3df7e0de821
何伟 <hewei@heweis-Mac-mini.local>
2020-06-10 14:35:14 +0800

    case

--- Element.py
+++ Element.py
@@ -3,3 +3,9 @@
 
 #密码登录
 onekey_register_topwd="com.jsmapp.jsm:id/onekey_register_topwd"
+et_UserName="com.jsmapp.jsm:id/et_UserName"
+et_Password="com.jsmapp.jsm:id/et_Password"
+loginBtn="com.jsmapp.jsm:id/loginBtn"
+iv_me_normal="com.jsmapp.jsm:id/ll_me"
+ll_immediate="com.jsmapp.jsm:id/ll_immediate"
+
--- Test_case.py
+++ Test_case.py
@@ -6,17 +6,32 @@ import Config
 import Element
 import pytest
 import time
-from flaky import flaky
+#from flaky import flaky
 
 class Test():
+    def wait(self):
+        self.driver.implicitly_wait(15)
+
+
     def startapp(self):
         self.driver = webdriver.Remote('http://0.0.0.0:4444/wd/hub', Config.desired_caps)
 
     @pytest.mark.case()
-    @flaky(max_runs=3, min_passes=1)
+    #@flaky(max_runs=3, min_passes=1)
     def test_pwdlogin(self):
         self.startapp()
         self.driver.find_element_by_id(Element.onekey_register_topwd).click()
+        #self.driver.implicitly_wait(15)
+        self.driver.find_element_by_id(Element.et_UserName).send_keys("15675456460")
+        #self.driver.implicitly_wait(15)
+        self.driver.find_element_by_id(Element.et_Password).send_keys("a111111")
+        #self.driver.implicitly_wait(15)
+        self.driver.find_element_by_id(Element.loginBtn).click()
+        self.driver.implicitly_wait(15)
+        self.driver.find_element_by_id(Element.iv_me_normal).click()
+        self.driver.implicitly_wait(15)
+        self.driver.find_element_by_id(Element.ll_immediate).click()
+
         print("pass")
 
 
