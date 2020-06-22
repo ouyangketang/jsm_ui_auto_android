f74c59ea536f9db3ae83ce8939c9959104913fa6
ouyangketang <1137222499@qq.com>
2020-06-22 15:39:52 +0800

    shanchubaogao

--- case/login_case/test_one_key_login.py
+++ case/login_case/test_one_key_login.py
@@ -2,18 +2,19 @@ from common.start import start_app
 from page.login_module.one_key_login import One_Key_login
 from flaky import flaky
 import pytest
+import allure
 class Test_One_Key_Login():
 
     @pytest.mark.po()
     @flaky(max_runs=1,min_passes=1)
+    @allure.story("第一条一键登录用例")
     def test_login01(self):
-        '''第一条一键登录用例'''
         app = start_app()
         test_login = One_Key_login(app)     # 调用登录的类
         test_login.one_key_login01()        # 调用第一个登录函数
 
+    @allure.story("第二条一键登录用例")
     def test_login02(self):
-        '''第二条一键登录用例'''
         app = start_app()
         test_login = One_Key_login(app)     # 调用登录的类
         test_login.one_key_login02()        # 调用第二个登录函数
